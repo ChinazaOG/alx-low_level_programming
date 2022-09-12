90de9446ab8595268b64ec22bc858af87459638f
ChinazaOG <chinazauwaomah@gmail.com>
2022-09-12 04:18:25 -0700

    content in

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long int: %ld bytes(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
