d5e15b5bc44090b23286cb63fd43598210dca5a2
ChinazaOG <chinazauwaomah@gmail.com>
2022-09-12 06:00:57 -0700

    join it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,16 @@
 
 int main(void)
 {
-	printf("size of a char: %i byte(s)\n", sizeof(char));
-	printf("size of a int: %i byte(s)\n", sizeof(int));
-	printf("size of a float %i byte(s)\n", sizeof(float));
-	printf("size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
+	char goat = 0;
+	int kettle = 0;
+	long int baby = 0;
+	long long int cowleg = 0;
+	float monney = 0;
+
+	printf("size of a char: %i byte(s)\n", sizeof(goat));
+	printf("size of a int: %i byte(s)\n", sizeof(kettle));
+	printf("size of a float %i byte(s)\n", sizeof(monney));
+	printf("size of a long int: %i byte(s)\n", sizeof(baby));
+	printf("size of a long long int: %i byte(s)\n", sizeof(cowleg));
 	return (0);
 }
