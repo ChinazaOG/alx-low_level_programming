6c6b57d84bfb079893ee658e94630873962fe389
ChinazaOG <chinazauwaomah@gmail.com>
2022-09-12 06:05:22 -0700

    join it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char goat = 0;
 	int kettle = 0;
+	float monney =0;
 	long int baby = 0;
 	long long int cowleg = 0;
-	float monney = 0;
-
+	
 	printf("size of a char: %i byte(s)\n", sizeof(goat));
 	printf("size of a int: %i byte(s)\n", sizeof(kettle));
 	printf("size of a float %i byte(s)\n", sizeof(monney));
