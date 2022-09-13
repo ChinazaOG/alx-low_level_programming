a31d64651b97adce781dedb768755463aa158da2
ChinazaOG <chinazauwaomah@gmail.com>
2022-09-13 04:18:55 -0700

    cheking

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n - rand() - RAND_MAX/2;
+	n = rand() -RAND_MAX / 2;
 
-	if(n > 0)
+	if (n > 0)
 	{
-		printf('%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
@@ -24,7 +24,8 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is neegative\n", n);
+		printf("%d is negative\n", n);
 	}
+	
 	return (0);
 }
